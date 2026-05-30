ec2c059fe325e7dbd5e55f6baceffbfaa4aba301
wyf <131146606+wyf-nlp@users.noreply.github.com>
2026-05-30 22:34:45 +0800

    Update infer_ram.sh

--- scripts/infer_ram.sh
+++ scripts/infer_ram.sh
@@ -1,9 +1,8 @@
 # Learning rate 
 LR=2e-5
 
-# seed
-# SEEDS=(22 42 66 99 111 1234)
-SEEDS=(10)
+# Seed
+SEEDS=(22 42 66 99 111 1234)
 for SEED in "${SEEDS[@]}"
 do
 
@@ -18,9 +17,6 @@ do
         --inference_model_path exps/rams/$SEED/checkpoint \
         --role_path=./data/dset_meta/description_rams.csv \
         --ontology_path=./data/templates/ontology_rams_full.csv \
-        --template_path1=./data/templates/templates_rams_full_t1.csv \
-        --template_path2=./data/templates/templates_rams_full_t3.csv \
-        --template_path3=./data/templates/templates_rams_full_t5.csv \
         --seed=$SEED \
         --output_dir=$work_path \
         --learning_rate=$LR \
@@ -28,11 +24,9 @@ do
         --max_steps=10000 \
         --max_enc_seq_length 500 \
         --max_template_seq_length 210 \
-        --device cuda:3 \
         --bipartite \
-        --lamb 1e-5 \
         --use_arg_moe \
         --moe_num_experts 4 \
         --moe_top_k 1 \
-        --lambd 0.001
+        --lambd 0.1
 done
