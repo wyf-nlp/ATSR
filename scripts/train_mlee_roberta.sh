fd6cdb711ef8653039b7d98d7cbb064e1a7f5148
wyf <131146606+wyf-nlp@users.noreply.github.com>
2026-05-30 22:28:44 +0800

    Update train_mlee_roberta.sh

--- scripts/train_mlee_roberta.sh
+++ scripts/train_mlee_roberta.sh
@@ -1,32 +1,34 @@
-# Learning rate 固定
+# Learning rate 
 LR=3e-5
 
-# 定义种子数组
+# seed
 SEEDS=(22 42 66 99 111 1234)
-# SEEDS=(111)
-# 遍历种子数组
+
 for SEED in "${SEEDS[@]}"
 do
-    # 设置工作路径
-    work_path=exp/mlee/$SEED
+
+    work_path=exps/mlee/$SEED
     mkdir -p $work_path
 
-    # 运行 Python 脚本
     python -u engine.py \
         --dataset_type=MLEE \
         --context_representation=decoder \
         --model_name_or_path=roberta-large \
         --role_path=./data/MLEE/MLEE_role_name_mapping.json \
-        --prompt_path=./data/prompts/prompts_MLEE_full.csv \
+        --ontology_path=./data/templates/ontology_MLEE_full.csv \
         --seed=$SEED \
         --output_dir=$work_path \
         --learning_rate=$LR \
         --batch_size=4 \
         --max_steps=10000 \
         --max_enc_seq_length 500 \
-        --max_dec_seq_length 360 \
+        --max_template_seq_length 360 \
         --window_size 250 \
         --warmup_steps 0.2 \
         --bipartite \
-        --lamb 0.1
+        --lamb 0.1 \
+        --use_arg_moe \
+        --moe_num_experts 5 \
+        --moe_top_k 1 \
+        --lambd 0.1
 done
