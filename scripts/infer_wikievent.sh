f14fac969cbab5d591798ab72903db5a544ebe2c
wyf <131146606+wyf-nlp@users.noreply.github.com>
2026-05-30 22:35:29 +0800

    Update infer_wikievent.sh

--- scripts/infer_wikievent.sh
+++ scripts/infer_wikievent.sh
@@ -1,9 +1,8 @@
 # Learning rate 
 LR=3e-5
 
-# seed
-# SEEDS=(22 42 66 99 111 1234)
-SEEDS=(1234)
+# Seed
+SEEDS=(22 42 66 99 111 1234)
 for SEED in "${SEEDS[@]}"
 do
 
@@ -18,9 +17,6 @@ do
         --inference_model_path exps/wikievent/$SEED/checkpoint \
         --role_path=./data/dset_meta/description_wikievent.csv \
         --ontology_path=./data/templates/ontology_wikievent_full.csv \
-        --template_path1=./data/templates/templates_wikievent_full_t1.csv \
-        --template_path2=./data/templates/templates_wikievent_full_t2.csv \
-        --template_path3=./data/templates/templates_wikievent_full_t4.csv \
         --seed=$SEED \
         --output_dir=$work_path \
         --learning_rate=$LR \
@@ -28,8 +24,6 @@ do
         --max_steps=10000 \
         --max_enc_seq_length 500 \
         --max_template_seq_length 360 \
-        --window_size 250 \
-        --device cuda:3 \
         --bipartite \
         --lamb 0.1 \
         --use_arg_moe \
